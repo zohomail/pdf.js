5875cddbf0729f3352bb5bc35ae8c569114ed138
Tim van der Meij <timvandermeij@gmail.com>
2026-02-14 20:12:43 +0100

    Upgrade `postcss-dir-pseudo-class` to version 10.0.0
    This is a major version bump, but the changelog at
    https://github.com/csstools/postcss-plugins/blob/main/plugins/postcss-dir-pseudo-class/CHANGELOG.md
    doesn't indicate any breaking changes that should impact us.
    
    We only have to list `postcss-dir-pseudo-class` in `eslint.config.js`
    similar to e.g. commit a2909f9b.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -118,6 +118,7 @@ export default [
             "web",
             "fluent-bundle",
             "fluent-dom",
+            "postcss-dir-pseudo-class",
             "postcss-nesting",
             "postcss-values-parser",
             "stylelint",
